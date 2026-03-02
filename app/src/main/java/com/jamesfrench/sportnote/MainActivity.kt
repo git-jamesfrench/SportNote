40e23fa7509eb53cbdf5f554b302503f0ff8ed3f
Jamesfrench <119378901+git-jamesfrench@users.noreply.github.com>
2026-03-02 23:11:13 +0100

    Added Navigation & Optimizations

--- app/src/main/java/com/jamesfrench/sportnote/MainActivity.kt
+++ app/src/main/java/com/jamesfrench/sportnote/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.jamesfrench.sportnote
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jamesfrench.sportnote.ui.theme.SportNoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +41,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             SportNoteTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        Navigation()
+                        Column() { // Content
+                            AddButton()
+                        }
+                    }
                 }
             }
         }
@@ -31,17 +54,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AddButton(modifier: Modifier = Modifier) {
+    Button(
+        onClick = {},
+        modifier = Modifier
+            .size(75.dp),
+        contentPadding = PaddingValues(0.dp),
+        shape = RoundedCornerShape(18.dp),
+        elevation = null
+    ) {
+        Icon(
+            painterResource(R.drawable.plus),
+            contentDescription = "Plus icon",
+            modifier = Modifier.size(30.dp),
+            tint = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+}
+
+@Composable
+fun Navigation(modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(17.dp, 17.dp, 17.dp, 0.dp),
+        horizontalArrangement = Arrangement.spacedBy(17.dp),
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(75.dp)
+                .background(MaterialTheme.colorScheme.primaryContainer, RoundedCornerShape(18.dp))
+        ) {
+
+        }
+        AddButton()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SportNoteTheme {
-        Greeting("Android")
+        Navigation()
     }
 }
--- app/src/main/java/com/jamesfrench/sportnote/ui/theme/Theme.kt
+++ app/src/main/java/com/jamesfrench/sportnote/ui/theme/Theme.kt
@@ -9,19 +9,27 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF1A34DA),
+    onPrimary = Color(color=0xFFFFFFFF),
+
+    primaryContainer = Color(color = 0xFF1E2026),
+    onPrimaryContainer = Color(color=0xFFFFFFFF),
+
+    background = Color(color=0xFF121318),
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF1A34DA),
+    onPrimary = Color(color = 0xFFFFFFFF),
 
+    primaryContainer = Color(color = 0xFFCDCDD5),
+    onPrimaryContainer = Color(color = 0xFF000000),
+
+    background = Color(color = 0xFFE6E7ED),
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -37,7 +45,7 @@ private val LightColorScheme = lightColorScheme(
 fun SportNoteTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
