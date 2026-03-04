88e4f08e12e1b58863da02d10fbdeee43174a35d
Jamesfrench <119378901+git-jamesfrench@users.noreply.github.com>
2026-03-04 23:03:54 +0100

    Added content

--- app/src/main/java/com/jamesfrench/sportnote/MainActivity.kt
+++ app/src/main/java/com/jamesfrench/sportnote/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jamesfrench.sportnote
 
+import android.R.attr.fontWeight
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,8 +12,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -29,9 +32,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jamesfrench.sportnote.ui.theme.SportNoteTheme
+import com.jamesfrench.sportnote.ui.theme.fontInter
+import com.jamesfrench.sportnote.ui.theme.fontJost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,14 +50,102 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)) {
                         Navigation()
-                        Column(
-                            modifier = Modifier.padding(17.dp, 0.dp, 17.dp, 0.dp)
-                        ) { // Content
-                            Text("Hello, World!")
-                        }
+                        MainPage()
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MainPage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(17.dp, 17.dp, 17.dp, 0.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Icon(
+                painterResource(R.drawable.notebook_dumbbell),
+                contentDescription = stringResource(R.string.app_name),
+                modifier = Modifier.size(35.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+            Text(
+                stringResource(R.string.app_name),
+                fontFamily = fontJost,
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+        Text(
+            stringResource(R.string.recent),
+            color = MaterialTheme.colorScheme.onSurface,
+            fontFamily = fontJost
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(2.dp, RoundedCornerShape(18.dp))
+                .background(MaterialTheme.colorScheme.surfaceContainer, RoundedCornerShape(18.dp))
+                .padding(17.dp, 12.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Exercise()
+        }
+    }
+}
+
+@Composable
+fun Exercise() {
+    Row(
+        modifier = Modifier.
+            height(30.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(14.dp)
+    ) {
+        Icon(
+            painterResource(R.drawable.dumbbell),
+            contentDescription = "Musculation exercise",
+            modifier = Modifier.size(19.dp),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            "Musculation Titre",
+            fontFamily = fontJost,
+            fontSize = 18.sp,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                "10",
+                fontFamily = fontJost,
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                "×",
+                fontFamily = fontJost,
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.onSurface,
+                fontWeight = FontWeight.SemiBold
+            )
+            Text(
+                "4s",
+                fontFamily = fontJost,
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
         }
     }
 }
@@ -61,7 +156,7 @@ fun AddButton(onClick: () -> Unit, icon: Int, iconDescription: String, modifier:
         onClick = onClick,
         modifier = modifier
             .size(75.dp)
-            .shadow(2.dp, RoundedCornerShape(18.dp)),
+            .shadow(4.dp, RoundedCornerShape(18.dp)),
         contentPadding = PaddingValues(0.dp),
         shape = RoundedCornerShape(18.dp),
         elevation = null
@@ -116,7 +211,7 @@ fun Navigation(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .weight(1f)
                 .height(75.dp)
-                .shadow(2.dp, RoundedCornerShape(18.dp))
+                .shadow(4.dp, RoundedCornerShape(18.dp))
                 .background(MaterialTheme.colorScheme.surfaceContainer, RoundedCornerShape(18.dp))
                 .padding(5.dp),
             horizontalArrangement = Arrangement.spacedBy(5.dp)
@@ -130,10 +225,11 @@ fun Navigation(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = false, heightDp = 180)
+@Preview(showBackground = true, backgroundColor = 0xFF121318)
 @Composable
 fun GreetingPreview() {
     SportNoteTheme(darkTheme = true) {
         Navigation()
+        MainPage()
     }
 }
---
+++ app/src/main/java/com/jamesfrench/sportnote/ui/theme/Font.kt
@@ -0,0 +1,17 @@
+package com.jamesfrench.sportnote.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.jamesfrench.sportnote.R
+
+val fontInter = FontFamily(
+    Font(R.font.inter)
+)
+
+val fontJost = FontFamily(
+    Font(
+        R.font.jost,
+    )
+)
